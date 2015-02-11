9419e1b6d848fbbb8c6cb7068a9b29cc073083cc
Justin Yoo <justin.yoo@aliencube.com>
2015-02-12 10:23:31 +1100

    Cleanse Global.asax.cs

--- SourceCodes/02_Apps/JwtAuthentication.WebApp/Global.asax.cs
+++ SourceCodes/02_Apps/JwtAuthentication.WebApp/Global.asax.cs
@@ -7,9 +7,6 @@ using System.Web.Security;
 
 namespace JwtAuthentication.WebApp
 {
-    using System.Linq;
-    using System.Security.Principal;
-
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
